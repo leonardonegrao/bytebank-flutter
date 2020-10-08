66dea9fcd9a5208fab08be0dcfa54cba847b0a9e
Leonardo Negrão <leonardolnegrao@gmail.com>
2020-10-08 20:09:25 -0300

    creates WireTransfer classes

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(
   MaterialApp(
     home: Scaffold(
-      body: Column(
-        children: [
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('100.0'),
-              subtitle: Text('1000'),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('200.0'),
-              subtitle: Text('2000'),
-            ),
-          ),
-        ],
-      ),
+      body: WireTransferList(),
       appBar: AppBar(
         title: Text('Transferências'),
       ),
@@ -31,4 +14,45 @@ void main() => runApp(
   ),
 );
 
+class WireTransferList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        WireTransferItem(new WireTransfer(100.0, "64321-0")),
+      ],
+    );
+  }
+}
 
+class WireTransferItem extends StatelessWidget {
+
+  final WireTransfer _wireTransfer;
+
+  const WireTransferItem(this._wireTransfer);
+
+  getValueAsCurrency() {
+    final valueAsString =  this
+        ._wireTransfer.value.toString();
+    final valueAsCurrency = 'R\$' + valueAsString;
+    return valueAsCurrency;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(getValueAsCurrency()),
+        subtitle: Text(_wireTransfer.accountNumber),
+      ),
+    );
+  }
+}
+
+class WireTransfer {
+  final double value;
+  final String accountNumber;
+
+  WireTransfer(this.value, this.accountNumber);
+}
